e735f8d615b67e9fc1eb34434eafaf788438e141
lorena <lugotlorenay@gmail.com>
2019-03-30 17:19:31 -0500

    agregando archivos y estructura general

---
+++ conection.js
@@ -0,0 +1,71 @@
+/* vars */
+let myForm;
+let refData;
+let resultsContainer = document.getElementById('search-results');
+const empresasBtn = document.getElementById('empresas-view');
+const queryBtn = document.getElementById('query-btn');
+const filterBtn = document.getElementById('empresas-button');
+/* functions */
+const inicializar = () => {
+	myForm = document.getElementById('form');
+	myForm.addEventListener('submit', enviarData, false);
+	refData = firebase.database().ref().child('data');
+	mostraDataBase();
+}
+
+const enviarData = (e) => {
+	e.preventDefault();
+	refData.push({
+		"Conductor": e.target.inputConductor.value,
+		"Descripción": e.target.inputDesc.value,
+		"Empresa": e.target.inputEmpresa.value,
+		"Fecha": e.target.inputFecha.value,
+		"Placa": e.target.inputPlaca.value
+	});
+	myForm.reset();
+}
+// for showing results DOM
+const mostraDataBase = () => {
+	refData.on('value', (snap) => {
+		let dataToShow = "";
+		let data = snap.val();
+		for(let key in data) {
+			dataToShow += `
+			<div class="row">
+				<div class="col-xs-6"><p>Datos: ${data[key]['Conductor']}</p><p>Placa: ${data[key]['Placa']}</p></div>
+				<div class="col-xs-12"><p>Fecha: ${data[key]['Fecha']}</p><p>Descripción: ${data[key]['Descripcion']}</p></div>
+			</div>`;
+		}
+		resultsContainer.innerHTML = dataToShow;
+		filterBtn.disabled = false;
+		filterBtn.innerText = "Empresas";
+	});
+}
+// filter database by empresas
+const filtrarDataBase = (condition) =>{
+	refData.on('value', (snap) => {
+		let data = snap.val();
+		let resultArr = [];
+		for(let key in data) {
+		if(data[key]['Empresa'] === condition) {
+			resultArr.push(data[key]);
+		}
+	}
+	return resultArr;
+	});
+}
+const displayBlock = (showId, hideId) => {
+	document.getElementById(hideId).classList.add('none');
+	document.getElementById(showId).classList.remove('none');
+}
+/* events */
+filterBtn.addEventListener('click', (e) => {
+	filterBtn.disabled = true;
+	filterBtn.innerText = "Cargando...";
+	inicializar();
+	displayBlock('empresas-view', 'main-view');
+});
+empresasBtn.addEventListener('click', (e) => {
+	displayBlock('search-results', 'empresas-view');
+});
+// window.onload = inicializar;
